d527cfd0061d36f7a8242cfc62e940f989757259
SurajKapu <98162452+SurajKapu@users.noreply.github.com>
2022-10-17 21:28:11 +0530

    Add files via upload

---
+++ src/student_results/Student_results.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student_results;
+
+/**
+ *
+ * @author suraj
+ */
+public class Student_results {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new homeIndex().setVisible(true);
+    }
+    
+}
